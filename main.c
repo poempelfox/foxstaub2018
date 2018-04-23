2bc1835fc876a5ed7f89f6db844b54a44e84ee30
Michael 'PoempelFox' Meier <git@mulder.franken.de>
2018-04-24 00:19:01 +0200

    query fine particle sensor, send packets with all data.

--- main.c
+++ main.c
@@ -29,13 +29,23 @@ uint32_t pressure = 0;
 int32_t temperature = 0;
 /* Rel. Humidity, in (% * 1024) */
 uint16_t humidity = 0;
+/* Particulate matter measurements. in (PMn * 10) ug/m^3 */
+/* 0xffff marks it as invalid, this value can never be measured as it's far
+ * outside the sensors range */
+uint16_t particulatematter2_5u = 0xffff;
+uint16_t particulatematter10u = 0xffff;
 
 /* This is just a fallback value, in case we cannot read this from EEPROM
  * on Boot */
 uint8_t sensorid = 3; // 0 - 255 / 0xff
 
 /* The frame we're preparing to send. */
-static uint8_t frametosend[12];
+static uint8_t frametosend[17];
+
+/* Length of one SDS011 measurement cycle, in ticks. */
+#define SDS011CYCLELENGTH 72 /* 151 seconds */
+/* How long do we turn the sensor on at the beginning of the cycle? */
+#define SDS011CYCLEONTIME 15 /* 31 seconds */
 
 /* We need to disable the watchdog very early, because it stays active
  * after a reset with a timeout of only 15 ms. */
@@ -79,8 +89,13 @@ static uint8_t calculatecrc(uint8_t * data, uint8_t len)
  * Byte  7: Pressure, LSB
  * Byte  8: Temperature, MSB   The temperature is in 1/100th degrees C with an
  * Byte  9: Temperature, LSB   offset of +100.00, so e.g. 12155 = 21.55 degC
- * Byte 10: 
- * Byte 11: CRC
+ * Byte 10: rel.Humidity, MSB     humidity is in 1/1024th percent.
+ * Byte 11: rel.Humidity, LSB
+ * Byte 12: PM2.5, MSB       particulate matter 2.5 is in 1/10th ug/m^3
+ * Byte 13: PM2.5, LSB
+ * Byte 14: PM10, MSB
+ * Byte 15: PM10, LSB
+ * Byte 16: CRC
  */
 void prepareframe(void)
 {
@@ -89,7 +104,7 @@ void prepareframe(void)
   if (temperature < -10000) { temptmp = 0; }
   frametosend[ 0] = 0xCC;
   frametosend[ 1] = sensorid;
-  frametosend[ 2] = 8; /* 8 bytes of data follow (CRC not counted) */
+  frametosend[ 2] = 13; /* 13 bytes of data follow (CRC not counted) */
   frametosend[ 3] = 0xf5; /* Sensor type: FoxStaub */
   frametosend[ 4] = (pressure >> 24) & 0xff;
   frametosend[ 5] = (pressure >> 16) & 0xff;
@@ -97,8 +112,13 @@ void prepareframe(void)
   frametosend[ 7] = (pressure >>  0) & 0xff;
   frametosend[ 8] = (temptmp >> 8) & 0xff;
   frametosend[ 9] = (temptmp >> 0) & 0xff;
-  frametosend[10] = 0;
-  frametosend[11] = calculatecrc(frametosend, 11);
+  frametosend[10] = (humidity >> 8) & 0xff;
+  frametosend[11] = (humidity >> 0) & 0xff;
+  frametosend[12] = (particulatematter2_5u >> 8) & 0xff;
+  frametosend[13] = (particulatematter2_5u >> 0) & 0xff;
+  frametosend[14] = (particulatematter10u >> 8) & 0xff;
+  frametosend[15] = (particulatematter10u >> 0) & 0xff;
+  frametosend[16] = calculatecrc(frametosend, 16);
 }
 
 void loadsettingsfromeeprom(void)
@@ -112,7 +132,9 @@ void loadsettingsfromeeprom(void)
 
 int main(void)
 {
-  uint16_t lastts = 0xf000; /* This forces an update immediately after start */
+  uint16_t lasttxts = 0xf000; /* This forces an update immediately after start */
+  uint16_t sds011cyclestart = 0xffff - (SDS011CYCLELENGTH / 2); /* places us at the end of a cycle */
+  uint16_t lastloopts = 0;
   uint16_t curts;
   uint16_t tsdiff;
   uint8_t transmitinterval = 15; /* Transmitinterval in ticks of 2.1s, so 15 = 31s */
@@ -130,8 +152,6 @@ int main(void)
   rfm69_setsleep(1);
   /* The BME280 needs 2 ms startup, so should be ready now as well. */
   bme280_init();
-  /* As should the SDS011 */
-  sds011_init();
   
   /* Enable watchdog timer with a timeout of 8 seconds */
   wdt_enable(WDTO_8S); /* Longest possible on ATmega328P */
@@ -141,6 +161,21 @@ int main(void)
    * is tristated on our side (it won't float, the RFM69 pulls it) */
   PORTE &= (uint8_t)~_BV(PE6);
   DDRE &= (uint8_t)~_BV(PE6);
+  /* Turn off unused stuff on the AVR via PRR registers */
+  /* We don't use Timer0 and the ADC*/
+  PRR0 |= _BV(PRTIM0) | _BV(PRADC);
+  /* We don't use Timer3+4. There seems to be a bug in
+   * avr-libc on Ubuntu 16.04, it doesn't define PRTIM4 but instead
+   * PRTIM2 for a nonexistant Timer2. Therefore we cannot use the
+   * macro and hardcode the correct value. */
+  PRR1 |= _BV(/* PRTIM4 */ 4) | _BV(PRTIM3);
+  DDRC |= (uint8_t)_BV(PC7); /* PC7 is the LED pin, drive it */
+  PORTC &= (uint8_t)~_BV(PC7); /* turn it off. */
+
+  /* The SDS011 needs a LOT longer to start. So lets sleep for some more
+   * time and then try to init it. */
+  _delay_ms(2000);
+  sds011_init();
 
   /* Prepare sleep mode */
   /* SLEEP_MODE_IDLE is the only sleepmode we can safely use. */
@@ -150,29 +185,27 @@ int main(void)
   /* All set up, enable interrupts and go. */
   sei();
 
-  DDRC |= (uint8_t)_BV(PC7); /* PC7 is the LED pin, drive it */
-  PORTC &= (uint8_t)~_BV(PC7); /* turn it off. */
-
   while (1) {
     wdt_reset();
     curts = timers_getticks();
-    tsdiff = curts - lastts;
+    tsdiff = curts - lasttxts;
     if (tsdiff >= transmitinterval) {
       /* Time to update values and send */
       bme280_readmeasuredvalues();
       pressure = bme280_getpressure();
       temperature = bme280_gettemperature();
       humidity = bme280_gethumidity();
-      
+      particulatematter2_5u = sds011_getlastpm2_5();
+      particulatematter10u = sds011_getlastpm10();
       bme280_startonemeasurement(); /* Start the next measurement */
       /* SEND */
       rfm69_setsleep(0);  /* This mainly turns on the oscillator again */
       prepareframe();
       console_printpgm_P(PSTR(" TX "));
-      rfm69_sendarray(frametosend, 12);
+      rfm69_sendarray(frametosend, 17);
       rfm69_setsleep(1);
       pktssent++;
-      lastts = curts; /* Remember when we last sent a packet */
+      lasttxts = curts; /* Remember when we last sent a packet */
       /* We use the lowest two bits of pressure as random noise */
       uint8_t rnd = pressure & 0x00000003;
       if (rnd == 3) {
@@ -181,6 +214,18 @@ int main(void)
         transmitinterval = 15;
       } else { /* 1 or 2 */
         transmitinterval = 16;
+      }
+    }
+    tsdiff = curts - lastloopts;
+    if (tsdiff > 0) { /* OK, we had one tick. (we need to make sure we execute only once per tick!) */
+      lastloopts = curts;
+      tsdiff = curts - sds011cyclestart;
+      if (tsdiff >= SDS011CYCLELENGTH) { /* Cycle over - start again */
+        sds011cyclestart = curts;
+        sds011_setmeasurements(1);
+      } else if (tsdiff == SDS011CYCLEONTIME) {
+        sds011_requestresult(); /* Request latest result */
+        sds011_setmeasurements(0); /* Then turn off */
       }
     }
     console_work();
